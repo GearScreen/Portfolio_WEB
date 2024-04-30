26898ea6d2aaa00d1f918ed93518326852993279
Yoan Lechevalier <yoan.lechevalier@gmail.com>
2024-04-30 11:52:19 +0200

    TranslateUpdate

---
+++ Gears.js
@@ -0,0 +1,21 @@
+const scrollTopValue = 100;
+
+// Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 80px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > scrollTopValue || document.documentElement.scrollTop > scrollTopValue) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
